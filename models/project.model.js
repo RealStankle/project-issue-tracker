71915c28857e49ca65df812b4bc1e768927f2d20
Furkan Emin <femincanx@gmail.com>
2023-01-20 12:52:45 +0300

    feat: add project model

---
+++ models/project.model.js
@@ -0,0 +1,19 @@
+const { model, Schema } = require('mongoose');
+
+const issueSchema = new Schema({
+  issue_title: { type: String, required: true },
+  issue_text: { type: String, required: true },
+  created_on: { type: Date, default: new Date() },
+  updated_on: { type: Date, default: new Date() },
+  created_by: { type: String, required: true },
+  assigned_to: String,
+  status_text: String,
+  open: { type: Boolean, default: true },
+});
+
+const projectSchema = new Schema({
+  name: String,
+  issues: [issueSchema],
+});
+
+module.exports = model('Project', projectSchema);
